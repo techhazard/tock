32121fc57c8542dd72a037c6ab082509afc4aec0
Philip Levis <pal@cs.stanford.edu>
2017-08-15 21:45:53 -0700

    Core of virtualization: a queue which provides a callback when the element is dequeued.

--- kernel/src/common/virtualizer.rs
+++ kernel/src/common/virtualizer.rs
@@ -5,6 +5,7 @@ use common::list::{List, ListLink, ListNode};
 
 pub trait Dequeued {
     fn dequeued(&self);
+    fn id(&self) -> u32;
 }
 
 pub struct QueuedCall<'a> {
@@ -34,6 +35,14 @@ impl<'a> QueuedCall<'a> {
         self.callback.set(Some(callback));
         self.queue.queued_calls.push_head(self);
     }
+
+    pub fn set_active(&'a self) {
+        self.active.set(true);
+    }
+
+    pub fn set_inactive(&'a self) {
+        self.active.set(false);
+    }
 }
 
 pub struct CallQueue<'a> {
@@ -49,44 +58,58 @@ impl<'a> CallQueue<'a> {
         }
     }
 
+    // This triggers the next queued element by performing
+    // a linear scan of the list, starting at the front. It
+    // keeps a reference to the 'next' element after the last one
+    // triggered (or None, if it was the tail of the queue).
+    // It keeps track of the 'last' active element before
+    // the 'next' element. It then walks forward from next,
+    // triggering the first active element. If it reaches the
+    // end of the queue, it triggers 'last' if there is one,
+    // or returns false if there is no 'last' (no element in the
+    // queue was marked active).
     pub fn dequeue_and_trigger(&self) -> bool {
+        // If the last scan reached the end of the queue,
+        // set the first element to look at to be the first element
+        // of the queue (could be None if queue is empty).
         if self.next.get().is_none() {
-            let mut next = false;
-            for call in self.queued_calls.iter() {
-                if next {
-                    self.next.set(Some(call));
-                    return true;
+            self.next.set(self.queued_calls.head());
+        }
+        let mut next = false; // Do we need to set next element?
+        let mut passed = false; // Have we passed next element?
+        let mut last = self.queued_calls.head(); // Last element 
+        for call in self.queued_calls.iter() {
+            if !passed {
+                if call as *const QueuedCall == self.next.get().unwrap() as *const QueuedCall {
+                    passed = true;
+                    self.next.set(None);
+                    if call.active.get() {
+                       next = true;
+                       call.active.set(false);
+                       call.callback.get().map(|c| c.dequeued());
+                    }
                 } else if call.active.get() {
-                    call.active.set(false);
-                    call.callback.get().map(|c| c.dequeued());
-                    next = true;
+                    last = Some(call);
                 }
             }
-            return next;
-        } else {
-            let mut next = false;
-            for call in self.next.get().iter() {
-                if next {
-                    self.next.set(Some(call));
-                    return true;
-                } else if call.active.get() {
-                    call.active.set(false);
-                    call.callback.get().map(|c| c.dequeued());
-                    next = true;
-                }
-            }
-            self.next.set(None);
-            // Fired, but it was the last element of the list
-            // Since self.next is None we'll start at beginning next time.
-            if next == true {
+            else if next {
+                self.next.set(Some(call));
                 return true;
-            }
-            // Didn't fire, so try wrapping around to beginning of list.
-            if next == false {
-                return self.dequeue_and_trigger();
+            } else if call.active.get() {
+                call.active.set(false);
+                call.callback.get().map(|c| c.dequeued());
+                next = true;
+                self.next.set(None);
             }
         }
-        false
+        if last.is_some() {
+            let val = last.unwrap();
+            if val.active.get() {
+                val.active.set(false);
+                val.callback.get().map(|c| c.dequeued());
+                return true;
+            }
+        }
+        next
     }
-
 }
